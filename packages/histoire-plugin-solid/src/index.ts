41dcaf91d02ae68de3813df455377371d4d41204
Miles Johnson <mileswjohnson@gmail.com>
2022-10-05 19:00:57 -0700

    Scaffold package.

---
+++ packages/histoire-plugin-solid/src/index.ts
@@ -0,0 +1,7 @@
+import type { Plugin } from 'histoire';
+
+export function HstSolid(): Plugin {
+	return {
+		name: 'histoire-plugin-solid',
+	};
+}
---
+++ web/histoire.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'histoire';
+import { HstSolid } from 'histoire-plugin-solid';
+
+export default defineConfig({
+	plugins: [HstSolid()],
+});
